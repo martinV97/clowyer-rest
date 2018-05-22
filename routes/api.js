b4805a45437012f5520ab90cb78fd61879aab725
Martin <martin.vn11@gmail.com>
2018-05-21 20:04:36 -0500

    prueba de alertas

--- routes/api.js
+++ routes/api.js
@@ -19,24 +19,19 @@ router.get('/', function(req, res, next) {
 
 router.post('/login-lawyer-web', function(req, res, next) {
 	Lawyer.findOne({email: req.body.email}).then(function(Lawyer){
-		if(!Lawyer){
-			alert("Correo no encontrado");
-			res.sendFile('login.html', {root: 'views'});
-		}else{
-			bcrypt.compare(req.body.password, Lawyer.password, function(err, result) {
-				if(err){
-					JSAlert.alert("Error de inicio de sesión");
-					res.sendFile('login.html', {root: 'views'});
-				}
-				if(result){
-					req.session.lawyer = Lawyer;
-					res.sendFile('main.html', {root: 'views'});
-				}else{
-					JSAlert.alert("Contraseña erronea");
-					res.sendFile('login.html', {root: 'views'});
-				}
-			});
-		}
+		bcrypt.compare(req.body.password, Lawyer.password, function(err, result) {
+			if(err){
+				JSAlert.alert("Error de inicio de sesión");
+				res.sendFile('login.html', {root: 'views'});
+			}
+			if(result){
+				req.session.lawyer = Lawyer;
+				res.sendFile('main.html', {root: 'views'});
+			}else{
+				alert("Contraseña erronea");
+				res.sendFile('login.html', {root: 'views'});
+			}
+		});
 	});
 });
 
