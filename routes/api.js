288f04c69e8cbc56c567387a1e57c6e690c6b92a
Martin <martin.vn11@gmail.com>
2018-05-24 20:39:57 -0500

    test nombre usuario y imagen redimensionada

--- routes/api.js
+++ routes/api.js
@@ -55,7 +55,9 @@ router.get('/main', function(req, res, next) {
     		Client.find({idLawyer: req.session.lawyer._id}).then(function(Client){
     			Court.find({}).then(function(Court){
     				res.render('main',{Case: Case, Client: Client, Court: Court,
-    				 idLawyer: req.session.lawyer._id, avatar: req.session.lawyer.avatar});		
+    				 idLawyer: req.session.lawyer._id,
+    				 avatar: req.session.lawyer.avatar,
+    				 name: req.session.lawyer.name});		
 				});
 			});
 		});		
