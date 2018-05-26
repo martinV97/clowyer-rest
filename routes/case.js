12818e48192c67de89932e3b3ad8ac3f641f6fdb
Martin <martin.vn11@gmail.com>
2018-05-26 16:47:00 -0500

    prueba actualizar caso

--- routes/case.js
+++ routes/case.js
@@ -53,11 +53,9 @@ router.put('/case/:id', function(req, res, next){
 });
 
 router.post('/case-web-update/', function(req, res, next){
-	var id = req.body._id;
-	req.body.id = null;
-	console.log(req.body);
-	Case.findByIdAndUpdate(id, req.body, (err, todo) => {}).then(function(Case){
-		 res.redirect('/details/' + req.params.id);
+	re.body.idLawyer = req.session.lawyer._id;
+	Case.findByIdAndUpdate(req.session.temporalCase._id, req.body, (err, todo) => {}).then(function(Case){
+		 res.redirect('/details/' + req.session.temporalCase.number);
 	});
 });
 
