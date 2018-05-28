6b1c1f1ec97b7bf971c3dd4709380e4a6eb1b707
Martin <martin.vn11@gmail.com>
2018-05-28 10:47:38 -0500

    Busqueda caso

--- public/js-css/js/case.js
+++ public/js-css/js/case.js
@@ -19,6 +19,24 @@ function deleteCase(id){
   });
   location.reload();
 }
+
+function searchFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchCase");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tableDoc");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
 /*$("button[name*='caseForm']").click(  function () {
   var sendData = $("form[name*='formCase']").serializeArray();
   console.log(sendData);
